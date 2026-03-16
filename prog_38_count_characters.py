281a50433e664aebaf570f995a38d79c90059b38
markivanflorentino05-rgb <markivanflorentino05@gmail.com>
2026-03-16 12:00:00 +0800

    Count and print the number of characters

---
+++ prog_38_count_characters.py
@@ -0,0 +1,5 @@
+# Ask user to input fullname, print the number of characters (including spaces).
+
+full_name = input("Enter your fullname: ")
+character_count = len(full_name)
+print(character_count)
